02bd65e0442a4a36d07dece53563cc1c99d2037e
Chana Barenholtz <chanabarenholtz@gmail.com>
2022-05-01 22:34:23 -0400

    Add message

--- April27/ClientApp/src/components/Peopletable.js
+++ April27/ClientApp/src/components/Peopletable.js
@@ -15,7 +15,7 @@ class PeopleTable extends Component {
     }
     onTextChange = e => {
         const person = { ...this.state.person };
-        person[e.target.name] =  e.target.value;
+        person[e.target.name] = e.target.value;
         this.setState({ person });
     }
 
@@ -28,27 +28,30 @@ class PeopleTable extends Component {
         this.setState({ people: [] });
     }
     generateBody() {
-        return this.state.people.map((p, i) => <PersonRow key={i} person={p} />)
+        if (this.state.people.length == 0) {
+            return <h1>No people yet. Add people to fill table.</h1>
+        }
+        return <table className='table table-hover table-striped table-bordered mt-3'>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Age</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.people.map((p, i) => <PersonRow key={i} person={p} />)}
+            </tbody>
+        </table>
     }
     render() {
         return (
-            <div className='container col-md-8'>
+            <div className='container col-md-9'>
                 <PersonForm person={this.state.person}
                     onTextChange={this.onTextChange}
                     onClickAdd={this.onClickAdd}
                     onClickClear={this.onClickClear}></PersonForm>
-                <table className='table table-hover table-striped table-bordered mt-3'>
-                    <thead>
-                        <tr>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Age</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.generateBody()}
-                    </tbody>
-                </table>
+                {this.generateBody()}
             </div>
         );
     }
